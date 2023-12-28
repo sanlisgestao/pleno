a27558af6357e2e11d5604e6a200932508ba5fd2
Angelita Santos <angelitasantos@gmail.com>
2023-12-27 21:51:59 -0300

    template pleno

--- static/painel/js/modules/show-data-reg-comex.js
+++ static/painel/js/modules/show-data-reg-comex.js
@@ -5,9 +5,9 @@ $(document).ready(function () {
 
 
 
-const GoogleRegistros = 'AKfycbwHYuCyZQ1eeg_2pUP2wfTM4t3C26MqzpQLmUEuXTaMaDOogmwnmVFoY0J9D38PtgzLXQ';
+const GoogleRegistros = 'AKfycbx-VBkpR0FPYVp2sOJLkStzXrxqri5dg1lFlnw79fH7LR4vEyMhx25WQfmqDWilxaa7TQ';
 // script file sig-reg-vendas
-var linkAction = 'https://script.google.com/macros/s/AKfycbwHYuCyZQ1eeg_2pUP2wfTM4t3C26MqzpQLmUEuXTaMaDOogmwnmVFoY0J9D38PtgzLXQ/exec';
+var linkAction = 'https://script.google.com/macros/s/AKfycbx-VBkpR0FPYVp2sOJLkStzXrxqri5dg1lFlnw79fH7LR4vEyMhx25WQfmqDWilxaa7TQ/exec';
 var linkFormAction = document.querySelector('#form-action');
 linkFormAction.setAttribute('action', linkAction);
 
@@ -42,9 +42,9 @@ function SearchRegistros(pNo = '') {
                 var i = 0;
                 $.each(record, function (key, value) {
                     if (i == 0) {
-                        var dt_registro = value[28].substring(0, 10);
-                        var dt_envio = value[29].substring(0, 10);
-                        var dt_vencto = value[30].substring(0, 10);
+                        var dt_registro = value[29].substring(0, 10);
+                        var dt_envio = value[30].substring(0, 10);
+                        var dt_vencto = value[31].substring(0, 10);
                         var dt_quitacao = value[33].substring(0, 10);
                         document.getElementsByName('inv_no')[0].value = value[0];
                         document.getElementsByName('ref_completa')[0].value = value[1];
@@ -53,7 +53,7 @@ function SearchRegistros(pNo = '') {
                         document.getElementsByName('nome_cliente')[0].value = value[4];
 
                         document.getElementsByName('ref_cliente')[0].value = value[6];
-                        document.getElementsByName('observacao')[0].value = value[7];
+                        document.getElementsByName('observacao')[0].value = value[19];
                         document.getElementsByName('status_pgto')[0].value = value[8];
                         document.getElementsByName('nf_1')[0].value = value[9];
                         document.getElementsByName('nf_2')[0].value = value[10];
@@ -61,25 +61,25 @@ function SearchRegistros(pNo = '') {
                         document.getElementsByName('nf_3')[0].value = value[11];
                         document.getElementsByName('nf_4')[0].value = value[12];
                         document.getElementsByName('nf_5')[0].value = value[13];
-                        document.getElementsByName('pref_1')[0].value = value[14];
-                        document.getElementsByName('pref_2')[0].value = value[15];
+                        document.getElementsByName('pref_1')[0].value = value[15];
+                        document.getElementsByName('pref_2')[0].value = value[16];
 
-                        document.getElementsByName('pref_3')[0].value = value[16];
-                        document.getElementsByName('pref_4')[0].value = value[17];
-                        document.getElementsByName('cnpj')[0].value = value[18];
-                        document.getElementsByName('razao_social')[0].value = value[19];
-                        document.getElementsByName('cod_parceiro')[0].value = value[24];
+                        document.getElementsByName('pref_3')[0].value = value[17];
+                        document.getElementsByName('pref_4')[0].value = value[18];
+                        document.getElementsByName('cnpj')[0].value = value[24];
+                        document.getElementsByName('razao_social')[0].value = value[25];
+                        document.getElementsByName('cod_parceiro')[0].value = value[26];
 
-                        document.getElementsByName('analista')[0].value = value[25];
-                        document.getElementsByName('conta')[0].value = value[26];
+                        document.getElementsByName('analista')[0].value = value[7];
+                        document.getElementsByName('conta')[0].value = value[27];
 
-                        document.getElementsByName('tipo_conta')[0].value = value[27];
+                        document.getElementsByName('tipo_conta')[0].value = value[28];
                         document.getElementsByName('data_registro')[0].value = dt_registro;
                         document.getElementsByName('data_envio')[0].value = dt_envio;
                         document.getElementsByName('data_vencto')[0].value = dt_vencto;
-                        document.getElementsByName('reg_total')[0].value = value[1];
+                        document.getElementsByName('reg_total')[0].value = value[14];
 
-                        document.getElementsByName('status_registro')[0].value = value[1];
+                        document.getElementsByName('status_registro')[0].value = value[32];
                         document.getElementsByName('data_quitacao')[0].value = dt_quitacao;
                     }
                     else {
